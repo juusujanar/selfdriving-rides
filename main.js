6fc58623d4663520e45b532dd4415e6efb518794
Andreas Baum <andreas.baum95@hotmail.com>
2018-12-28 11:25:19 +0200

    it does now automatically NxM road network

--- main.js
+++ main.js
@@ -1,28 +1,44 @@
-var app = new PIXI.Application(800, 600, {backgroundColor : 0xbbbcbf});
-document.body.appendChild(app.view);
+function drawRoads(rowCount, columnCount, maxColumnCount, maxRowCount){
+    // if you want to play with road/rectangle sizes then just adjust roadWidth and roadHeight
+    const roadWidth = 40 + 5 * (maxColumnCount - columnCount - 1);
+    const roadHeight = 40 + 5 * (maxRowCount - rowCount - 1);
 
-// creates the road network
-const rec_len = 120;
-const road_len = 40;
+    const rectWith = calcRectSize(800, columnCount, roadWidth);
+    const rectHeight = calcRectSize(600, rowCount, roadHeight);
 
-let y = -rec_len / 2;
-let rectangle = new PIXI.Graphics();
-for (let i = 0; i < 5; i++) {
-    let x = -rec_len / 2;
-    for (let j = 0; j < 6; j++) {
-        rectangle.beginFill(0xffffff);
-        rectangle.drawRect(x, y, rec_len, rec_len);
-        rectangle.endFill();
-        x = x + road_len + rec_len;
-        app.stage.addChild(rectangle);
+    let y = -rectHeight / 2;
+    let rectangle = new PIXI.Graphics();
+    for (let i = 0; i < rowCount + 1; i++) {
+        let x = -rectWith / 2;
+        for (let j = 0; j < columnCount + 1; j++) {
+            rectangle.beginFill(0xffffff);
+            rectangle.drawRect(x, y, rectWith, rectHeight);
+            rectangle.endFill();
+            x = x + roadWidth + rectWith;
+            app.stage.addChild(rectangle);
+        }
+        y = y + roadHeight + rectHeight;
     }
-    y = y + road_len + rec_len;
-    console.log(y)
+}
+
+function calcRectSize(canvasWidth, columnOrRowCount, roadSize) {
+    const unused = canvasWidth - columnOrRowCount * roadSize;
+    return unused / columnOrRowCount
 }
 
 
+
+
+
+var app = new PIXI.Application(800, 600, {backgroundColor : 0xbbbcbf});
+document.body.appendChild(app.view);
+
+drawRoads(10,8, 10, 8);
+
 // create a new Sprite from an image path
 var car = PIXI.Sprite.from("./car.png");
+car.scale.x = 0.4;
+car.scale.y = 0.4;
 
 
 // center the sprite's anchor point
@@ -39,7 +55,7 @@ app.ticker.add(function(delta) {
     // just for fun, let's rotate the vroom machine a little
     // delta is 1 if running at 100% performance
     // creates frame-independent transformation
-    car.rotation += 0.1 * delta;
+    //car.rotation += 0.1 * delta;
 });
 
 
